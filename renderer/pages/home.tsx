0e688a84fe13092ed1614682ade8593f280f2366
tiavina-mika <tiavinamika@gmail.com>
2022-06-03 08:42:19 +0200

    add form

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,28 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import Head from 'next/head';
 import Link from 'next/link';
 
-function Home() {
+const Home = () => {
+  const [notes, setNotes] = useState();
+
   return (
     <React.Fragment>
       <Head>
-        <title>Home - Nextron (with-typescript-tailwindcss)</title>
+        <title>Noteo</title>
       </Head>
-      <div className='grid grid-col-1 text-2xl w-full text-center'>
-        <img className='ml-auto mr-auto' src='/images/logo.png' />
-        <span>⚡ Electron ⚡</span>
-        <span>+</span>
-        <span>Next.js</span>
-        <span>+</span>
-        <span>tailwindcss</span>
-        <span>=</span>
-        <span>💕 </span>
-      </div>
-      <div className='mt-1 w-full flex-wrap flex justify-center'>
-        <Link href='/next'>
-          <a className='btn-blue'>Go to next page</a>
-        </Link>
-      </div>
+      <Link href="/add-note">
+        Ajouter nouveau note
+      </Link>
     </React.Fragment>
   );
 }
