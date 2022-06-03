6105fe56e24989521957c50172276514118038f1
tiavina-mika <tiavinamika@gmail.com>
2022-06-03 10:05:42 +0200

    add a form

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -3,13 +3,8 @@ import Head from 'next/head';
 import { Theme } from '@mui/material/styles';
 import makeStyles from '@mui/styles/makeStyles';
 import createStyles from '@mui/styles/createStyles';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogActions from '@mui/material/DialogActions';
 import Typography from '@mui/material/Typography';
+import Link from '../components/Link';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -20,39 +15,20 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-function Home() {
+const Home = () => {
   const classes = useStyles({});
-  const [open, setOpen] = React.useState(false);
-  const handleClose = () => setOpen(false);
-  const handleClick = () => setOpen(true);
 
   return (
     <React.Fragment>
       <Head>
-        <title>Home - Nextron (with-typescript-material-ui)</title>
+        <title>Noteo</title>
       </Head>
       <div className={classes.root}>
-        <Dialog open={open} onClose={handleClose}>
-          <DialogTitle>Super Secret Password</DialogTitle>
-          <DialogContent>
-            <DialogContentText>1-2-3-4-5</DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button color="primary" onClick={handleClose}>
-              OK
-            </Button>
-          </DialogActions>
-        </Dialog>
         <Typography variant="h4" gutterBottom>
-          Material-UI
+          <Link href="/notes/add">
+            Add new note
+          </Link>
         </Typography>
-        <Typography variant="subtitle1" gutterBottom>
-          with Nextron
-        </Typography>
-        <img src="/images/logo.png" />
-        <Button variant="contained" color="secondary" onClick={handleClick}>
-          Super Secret Password
-        </Button>
       </div>
     </React.Fragment>
   );
--- renderer/pages/notes/add.tsx
+++ renderer/pages/notes/add.tsx
@@ -1,40 +1,96 @@
-import React, { useState, ChangeEvent } from 'react';
+import {
+  Box,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { object, string, TypeOf } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useEffect, useState } from 'react';
+import { LoadingButton } from '@mui/lab';
 
-import Head from 'next/head';
+const noteSchema = object({
+  title: string()
+    .min(1, 'Title is required')
+    .max(100, 'Title must be less than 100 characters'),
+  content: string()
+    .min(1, 'Content is required')
+    .max(1000, 'Content must be less than 1000 characters'),
+})
 
-import { Note } from '../../types/note';
+type NoteInput = TypeOf<typeof noteSchema>;
 
-const AddNote = () => {
-  const [values, setValues] = useState<Note>({
-    title: '',
-    description: '',
+const Add = () => {
+  const [loading, setLoading] = useState(false);
+
+  const {
+    register,
+    formState: { errors, isSubmitSuccessful },
+    reset,
+    handleSubmit,
+  } = useForm<NoteInput>({
+    resolver: zodResolver(noteSchema),
   });
 
-  const onConfirm = () => {
-    console.log(values)
-  }
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isSubmitSuccessful]);
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setValues((prev) => ({
-      ...prev,
-      [e.target.name]: e.target.value,
-    }))
+  const onSubmitHandler: SubmitHandler<NoteInput> = (values) => {
+    console.log(values);
   };
 
   return (
-    <React.Fragment>
-      <Head>
-        <title>Add new note</title>
-      </Head>
-      <div className='grid grid-col-1 text-2xl w-full text-center'>
-        <form onSubmit={onConfirm}>
-          <input type="text" name="title" placeholder="Titre" value={values.title} onChange={handleChange} />
-          <input type="text" name="content" placeholder="Contenu" value={values.description} onChange={handleChange}/>
-          <button type="submit">Enregistrer</button>
-        </form>
-      </div>
-    </React.Fragment>
-  );
-}
+    <Box display="flex" flexDirection="column" alignItems="center" pt={5}>
+      <Box sx={{ maxWidth: '60vw' }} display="flex" flexDirection="column" alignItems="center">
+        <Typography variant='h4' component='h1' sx={{ mb: '2rem' }}>
+          Add new note
+        </Typography>
+        <Box
+          component='form'
+          noValidate
+          autoComplete='off'
+          onSubmit={handleSubmit(onSubmitHandler)}
+        >
+          <TextField
+            sx={{ mb: 2 }}
+            label='Title'
+            fullWidth
+            required
+            error={!!errors['title']}
+            helperText={errors['title'] ? errors['title'].message : ''}
+            {...register('title')}
+          />
+          <TextField
+            sx={{ mb: 2 }}
+            label='Content'
+            fullWidth
+            required
+            type='content'
+            error={!!errors['content']}
+            multiline
+            rows={6}
+            helperText={errors['content'] ? errors['content'].message : ''}
+            {...register('content')}
+          />
 
-export default AddNote;
+          <LoadingButton
+            variant='contained'
+            fullWidth
+            type='submit'
+            loading={loading}
+            sx={{ py: '0.8rem', mt: '1rem' }}
+          >
+            Save
+          </LoadingButton>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Add;
+
